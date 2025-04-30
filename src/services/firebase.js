126765a133e800a44b48068f7fbaac3ce6ff5361
Abhishek-Pandey786 <abhishek.pandey@mca.christuniversity.in>
2025-04-30 12:16:38 +0530

    Updated Firebase config to use environment variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,13 +5,13 @@ import { getDatabase } from 'firebase/database';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyATwAnjY4LUnFyrb54JxczHr3x-3Dd4IQE",
-  authDomain: "financetracker-7bc07.firebaseapp.com",
-  databaseURL: "https://financetracker-7bc07-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "financetracker-7bc07",
-  storageBucket: "financetracker-7bc07.firebasestorage.app",
-  messagingSenderId: "876727820710",
-  appId: "1:876727820710:web:2dac445e81072acbda6f67"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyATwAnjY4LUnFyrb54JxczHr3x-3Dd4IQE",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "financetracker-7bc07.firebaseapp.com",
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL || "https://financetracker-7bc07-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "financetracker-7bc07",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "financetracker-7bc07.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "876727820710",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:876727820710:web:2dac445e81072acbda6f67"
 };
 
 // Initialize Firebase
